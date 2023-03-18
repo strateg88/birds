82045bbb68ead6354542c53e62a47943846f3265
ivan <strateg8888@gmail.com>
2023-03-19 02:23:39 +0400

    Проверка + преобразование

--- src/main/java/org/muratoff/Main.java
+++ src/main/java/org/muratoff/Main.java
@@ -3,9 +3,21 @@ package org.muratoff;
 
 public class Main {
     public static void main(String[] args) {
-        Crow crow = new Crow();
-        Sparrow sparrow = new Sparrow();
-        Nightingale nightingale = new Nightingale();
+        Object obj1 = new Crow();
+        Object obj2 = new Sparrow();
+        Object obj3 = new Nightingale();
+        System.out.println(obj1 instanceof Crow);
+        System.out.println(obj1 instanceof Sparrow);
+        System.out.println(obj1 instanceof Nightingale);
+        System.out.println(obj2 instanceof Crow);
+        System.out.println(obj2 instanceof Sparrow);
+        System.out.println(obj2 instanceof Nightingale);
+        System.out.println(obj3 instanceof Crow);
+        System.out.println(obj3 instanceof Sparrow);
+        System.out.println(obj3 instanceof Nightingale);
+        Crow crow = (Crow) obj1;
+        Sparrow sparrow = (Sparrow) obj2;
+        Nightingale nightingale = (Nightingale) obj3;
 
         Object[] birds = new Object[3];
         birds[0] = crow;
