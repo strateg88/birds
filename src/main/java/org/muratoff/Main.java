73125ef9062c7f7451e718c6962470c9df69e59c
ivan <strateg8888@gmail.com>
2023-03-19 20:33:33 +0400

    Готово

--- src/main/java/org/muratoff/Main.java
+++ src/main/java/org/muratoff/Main.java
@@ -3,6 +3,7 @@ package org.muratoff;
 
 public class Main {
     int i;
+
     public static void main(String[] args) {
         Crow crow = new Crow();
         Sparrow sparrow = new Sparrow();
@@ -13,11 +14,25 @@ public class Main {
         birds[0] = crow;
         birds[1] = sparrow;
         birds[2] = nightingale;
-        if (crow instanceof Crow){
+        if (birds[0] instanceof Crow) {
             Crow castCrow = (Crow) birds[0];
         }
+
+
         for (int i = 0; i < birds.length; i++) {
-            System.out.println(birds[i]);
+            if (birds[i] instanceof Crow) {
+                Crow castCrow = (Crow) birds[i];
+                castCrow.song();
+            }
+            if (birds[i] instanceof Sparrow) {
+                Sparrow castSparrow = (Sparrow) birds[i];
+                castSparrow.song();
+            }
+            if (birds[i] instanceof Nightingale) {
+                Nightingale castNightingale = (Nightingale) birds[i];
+                castNightingale.song();
+
+            }
         }
     }
 }
